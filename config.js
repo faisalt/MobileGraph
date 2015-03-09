b533143d4255720a0e52414c2a563f84b211e691
faisalt <f.taher@lancaster.ac.uk>
2015-03-09 17:31:20 +0000

    Ongoing edits

--- config.js
+++ config.js
@@ -9,3 +9,6 @@ var protocol="http://";
 var host="148.88.227.240";
 var port="3000";
 
+//ShapeClip interface configuration
+var h_max = 5; //number of horizontal pixels, e.g. in 3x4 grid this would be 3
+var v_max = 6; // number of vertical pixels, e.g. in a 3x4 grid this would be 4
